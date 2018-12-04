8d64bc9edeb5835ab0f5b127c323c1bd4626a27a
Nicolas <nicolas-donald.laurent@laposte.net>
2018-12-04 01:10:48 +0100

    Still wip but nearer to make it works

--- CustomCar/CarBuilder2.cs
+++ CustomCar/CarBuilder2.cs
@@ -75,7 +75,191 @@ namespace CustomCar
         {
             var infos = new CreateCarReturnInfos();
 
+            var obj = GameObject.Instantiate(m_infos.baseCar);
+            obj.name = car.name;
+            GameObject.DontDestroyOnLoad(obj);
+            obj.SetActive(false);
+
+            removeOldCar(obj);
+            var newCar = addNewCarOnPrefab(obj, car);
+            setCarDatas(obj, newCar);
+
             return infos;
+        }
+
+        void removeOldCar(GameObject obj)
+        {
+            List<GameObject> wheelsToRemove = new List<GameObject>();
+            for (int i = 0; i < obj.transform.childCount; i++)
+            {
+                var c = obj.transform.GetChild(i).gameObject;
+                if (c.name.ToLower().Contains("wheel"))
+                    wheelsToRemove.Add(c);
+            }
+            if (wheelsToRemove.Count != 4)
+                ErrorList.add("Found " + wheelsToRemove.Count + " wheels on base prefabs, expected 4");
+
+            var refractor = obj.transform.Find("Refractor");
+            if (refractor == null)
+            {
+                ErrorList.add("Can't find the Refractor object on the base car prefab");
+                return;
+            }
+            GameObject.Destroy(refractor.gameObject);
+            foreach (var w in wheelsToRemove)
+                GameObject.Destroy(w);
+        }
+
+        GameObject addNewCarOnPrefab(GameObject obj, GameObject car)
+        {
+            return GameObject.Instantiate(car, obj.transform);
+        }
+
+        void setCarDatas(GameObject obj, GameObject car)
+        {
+            setColorChanger(obj.GetComponent<ColorChanger>(), car);
+            setCarVisuals(obj.GetComponent<CarVisuals>(), car);
+        }
+
+        void setColorChanger(ColorChanger colorChanger, GameObject car)
+        {
+            if(colorChanger == null)
+            {
+                ErrorList.add("Can't find the ColorChanger component on the base car");
+                return;
+            }
+
+            colorChanger.rendererChangers_ = new ColorChanger.RendererChanger[0];
+            foreach (var r in car.GetComponentsInChildren<Renderer>())
+            {
+                replaceMaterials(r);
+                if (colorChanger != null)
+                    addMaterialColorChanger(colorChanger, r.transform);
+            }
+        }
+
+        class MaterialPropertyExport
+        {
+            public enum PropertyType
+            {
+                Color,
+                ColorArray,
+                Float,
+                FloatArray,
+                Int,
+                Matrix,
+                MatrixArray,
+                Texture,
+                TextureOffset,
+                TextureScale,
+                Vector,
+                VectorArray,
+            }
+
+            public string fromName;
+            public string toName;
+            public int fromID = -1;
+            public int toID = -1;
+            public PropertyType type;
+        }
+
+        void replaceMaterials(Renderer r)
+        {
+            string[] matNames = new string[r.materials.Length];
+            for (int i = 0; i < matNames.Length; i++)
+                matNames[i] = "Wheel";
+            List<MaterialPropertyExport>[] matProperties = new List<MaterialPropertyExport>[r.materials.Length];
+            for (int i = 0; i < matProperties.Length; i++)
+                matProperties[i] = new List<MaterialPropertyExport>();
+
+            fillMaterialInfos(r, matNames, matProperties);
+
+            //todo change materials type from infos
+        }
+
+        void fillMaterialInfos(Renderer r, string[] matNames, List<MaterialPropertyExport>[] matProperties)
+        {
+            int childCount = r.transform.childCount;
+
+            for (int i = 0; i < childCount; i++)
+            {
+                var name = r.transform.GetChild(i).name.ToLower();
+                if (!name.StartsWith("#"))
+                    continue;
+                name = name.Remove(0, 1);
+                var s = name.Split(';');
+                if (s.Length == 0)
+                    continue;
+                if (s[0].Contains("mat"))
+                {
+                    if (s.Length != 3)
+                    {
+                        ErrorList.add(s[0] + " property on " + r.gameObject.FullName() + " must have 2 arguments");
+                        continue;
+                    }
+                    int index = 0;
+                    if (!int.TryParse(s[1], out index))
+                    {
+                        ErrorList.add("First argument of " + s[0] + " on " + r.gameObject.FullName() + " property must be a number");
+                        continue;
+                    }
+                    if (index < matNames.Length)
+                        matNames[index] = s[2];
+                }
+                else if (s[0].Contains("export"))
+                {
+                    if (s.Length != 5)
+                    {
+                        ErrorList.add(s[0] + " property on " + r.gameObject.FullName() + " must have 3 arguments");
+                        continue;
+                    }
+                    int index = 0;
+                    if (!int.TryParse(s[1], out index))
+                    {
+                        ErrorList.add("First argument of " + s[0] + " on " + r.gameObject.FullName() + " property must be a number");
+                        continue;
+                    }
+                    if (index >= matNames.Length)
+                        continue;
+                    MaterialPropertyExport p = new MaterialPropertyExport();
+                    bool found = false;
+
+                    foreach (MaterialPropertyExport.PropertyType pType in Enum.GetValues(typeof(MaterialPropertyExport.PropertyType)))
+                    {
+                        if (s[2] == pType.ToString().ToLower())
+                        {
+                            found = true;
+                            p.type = pType;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        ErrorList.add("The property " + s[2] + " on " + r.gameObject.FullName() + " is not valid");
+                        continue;
+                    }
+                    if (!int.TryParse(s[3], out p.fromID))
+                        p.fromName = s[3];
+                    if (!int.TryParse(s[4], out p.toID))
+                        p.toName = s[4];
+
+                    matProperties[index].Add(p);
+                }
+            }
+        }
+        
+        void addMaterialColorChanger(ColorChanger colorChanger, Transform t)
+        {
+
+        }
+
+        void setCarVisuals(CarVisuals visuals, GameObject car)
+        {
+            if(visuals == null)
+            {
+                ErrorList.add("Can't find the CarVisuals component on the base car");
+                return;
+            }
         }
     }
 }
